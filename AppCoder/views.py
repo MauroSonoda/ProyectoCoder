341e5e3f00c9ab63ee44125ef24c3bc3b24ec891
mauro sonoda <sonodamauro2.0@gmail.com>
2023-09-05 00:03:27 -0300

    first commit

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -9,3 +9,13 @@ def curso(req,nombre,camada):
 def lista_cursos(req):
     lista=Curso.objects.all()
     return render(req,"lista_cursos.html",{"lista_cursos":lista})#lista_cursos es la variable para que recorra en el template
+def inicio(req):
+    return render(req,"inicio.html")
+def cursos(req):
+    return render(req,"cursos.html")
+def profesores(req):
+   return render(req,"profesores.html")
+def estudiantes(req):
+    return render(req,"estudiantes.html")
+def entregables(req):
+   return render(req,"entregables.html")
